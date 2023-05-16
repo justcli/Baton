70c0d299975230c0f816085afd3388c7f7aba3eb
JustCLI <justcli@fastmail.com>
2023-05-16 21:06:12 +0530

    Updated install.sh to install for user requested python version.

--- install.sh
+++ install.sh
@@ -1,21 +1,28 @@
 #!/usr/bin/env bash
 
+if [ $# -ne 0 ]; then
+    py=$1
+else
+    py=python3
+fi
 ver=`/usr/bin/env python -c "from __future__ import print_function;import sys;print(sys.version[0])"`
 if [ $? -ne 0 ];then
 	echo "Could find any python installation."
 	exit 1
 fi
-pyexe=`which python`
+pyexe=`which $py`
+echo $py
+echo $pyexe
 if [ $ver -ne 3 ]; then
-	which python3 1>/dev/null 2>&1
+	which $py 1>/dev/null 2>&1
 	if [ $? -ne 0 ];then
-		echo "Could not find any python3 installation."
+		echo "Could not find any $1 installation."
 		exit 1
 	fi
 	pyexe=`which python3`
 fi
 
-import_path=`python3 -c "import sys;print(sys.path[-1] + '/baton')"`
+import_path=`$py -c "import sys;print(sys.path[-1] + '/baton')"`
 mkdir $import_path >/dev/null 2>&1
 cp -f __init__.py $import_path
 if [ $? -ne 0 ];then
@@ -25,4 +32,3 @@ if [ $? -ne 0 ];then
 fi
 
 echo "baton installed in "$import_path
-
